a81cb4220aadb9459eb24875834140ec16e0b980
masta-g3 <manolorueda@gmail.com>
2024-02-26 23:19:45 -0600

    batch

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if "openai_api_key" not in st.session_state:
     st.session_state.openai_api_key = ""
 
 if "all_years" not in st.session_state:
-    st.session_state.all_years = True
+    st.session_state.all_years = False
 
 
 collection_map = {
