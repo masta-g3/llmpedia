bb037c2825f2ad2f35b7e2badb67ad655b4c0755
masta-g3 <manolorueda@gmail.com>
2024-07-08 22:43:57 -0700

    data card generator

---
+++ workflow/e2_data_card.py
@@ -0,0 +1,42 @@
+import sys, os
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from tqdm import tqdm
+
+PROJECT_PATH = os.environ.get("PROJECT_PATH")
+sys.path.append(os.environ.get("PROJECT_PATH"))
+os.chdir(os.environ.get("PROJECT_PATH"))
+
+import utils.paper_utils as pu
+import utils.db as db
+import utils.prompts as p
+from utils.instruct import run_instructor_query
+
+def main():
+    arxiv_codes = pu.get_local_arxiv_codes()
+    done_codes = db.get_arxiv_id_list(db.db_params, "arxiv_dashboards")
+    arxiv_codes = list(set(arxiv_codes) - set(done_codes))
+    arxiv_codes = sorted(arxiv_codes)[::-1][:10]
+
+    for arxiv_code in tqdm(arxiv_codes):
+        title = db.get_arxiv_title_dict()[arxiv_code]
+        content = db.get_extended_notes(arxiv_code, expected_tokens=3000)
+        res_str = run_instructor_query(
+            p.DATA_CARD_SYSTEM_PROMPT,
+            p.PDATA_CARD_USER_PROMPT.format(title=title, content=content),
+            llm_model="claude-3-5-sonnet-20240620",
+            temperature=0.7
+        )
+        summary = res_str.split("<summary>")[1].split("</summary>")[0].strip()
+        script = res_str.split("<script>")[1].split("</script>")[0].strip()
+        scratchpad = ""
+        db.save_arxiv_dashboard_script(arxiv_code, summary, scratchpad, script)
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
